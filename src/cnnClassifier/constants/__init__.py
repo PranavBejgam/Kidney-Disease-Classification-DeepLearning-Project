af3a854a7c5843ca44550c43844b5dda277614dc
Pranav Bejgam <pranavbejgam26@gmail.com>
2025-07-25 13:58:17 +0530

    data ingestion added

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("C:/Users/Pranav Bejgam/Desktop/Kidney-Disease-Classification-DeepLearning-Project/config/config.yaml")
+PARAMS_FILE_PATH = Path("C:/Users/Pranav Bejgam/Desktop/Kidney-Disease-Classification-DeepLearning-Project/params.yaml")
+
+print(CONFIG_FILE_PATH)
